f6b4dde95990f3087a696252dde8af13ce8d70e0
Nitin Kumar Patel <niit.patel@gmail.com>
2021-12-17 13:58:41 -0600

    completed

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
-}
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 
-export default App;
+export default function App() {
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className="container">
+
+        <h4>{people.length} birthdays today</h4>
+
+        <List people={people} />
+        <button onClick={() =>
+          setPeople([])}>
+          clear
+        </button>
+      </section>
+
+    </main>
+  )
+}
